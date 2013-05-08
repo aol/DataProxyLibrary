eeca2b8772bae87b786a79068c9ac317b0c23970
sstrick <sstrick@adebfd7e-f233-4696-9a66-a9d0f0efb86a>
2013-05-08 21:23:22 +0000

    Updating version strings
    git-svn-id: svn+ssh://svn.cm.aol.com/advertising/adlearn/gen1/trunk/lib/cpp/DataProxy@277794 adebfd7e-f233-4696-9a66-a9d0f0efb86a

--- Shell/private/DataProxyShellConfig.cpp
+++ Shell/private/DataProxyShellConfig.cpp
@@ -21,7 +21,7 @@
 
 namespace
 {
-	const std::string SHELL_VERSION( "DataProxy Shell v3.1.9" );
+	const std::string SHELL_VERSION( "DataProxy Shell v3.1.10" );
 
 	const char* INIT( "init" );
 	const char* NAME( "name" );
