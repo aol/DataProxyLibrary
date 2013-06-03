bb33b93be5f2fee7182b5114bbf6cd0008b72a09
sstrick <sstrick@adebfd7e-f233-4696-9a66-a9d0f0efb86a>
2013-06-03 19:21:09 +0000

    Adding symlink for dpl 3.1 -> 3.1.10
    git-svn-id: svn+ssh://svn.cm.aol.com/advertising/adlearn/gen1/trunk/lib/cpp/DataProxy@280004 adebfd7e-f233-4696-9a66-a9d0f0efb86a

--- hcmset/scripts/post-install.sh
+++ hcmset/scripts/post-install.sh
@@ -37,6 +37,7 @@ mv ${TOPDIR}/libDataProxy.so.* ${LIBDIR}
 ln -sfn libDataProxy.so.${VERSION} ${LIBDIR}/libDataProxy.so.${MIN_VER}
 ln -sfn libDataProxy.so.${MIN_VER} ${LIBDIR}/libDataProxy.so.${MAJ_VER}
 ln -sfn libDataProxy.so.${MAJ_VER} ${LIBDIR}/libDataProxy.so
+ln -sfn libDataProxy.so.3.1.10 ${LIBDIR}/libDataProxy.so.3.1
 ln -sfn libDataProxy.so.3.0.2 ${LIBDIR}/libDataProxy.so.3.0
 for lib in `find ${TOPDIR} -name lib*.so.*`; do ln -sfn $(basename $lib) ${LIBDIR}/$(basename $lib .${VERSION}); done
 mv ${TOPDIR}/lib* ${LIBDIR}
