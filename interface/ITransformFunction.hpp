1ecb8ac0f13063f520b2cb5124537f2e777c69a3
esaxe <esaxe@adebfd7e-f233-4696-9a66-a9d0f0efb86a>
2013-06-19 22:02:15 +0000

    Missed adding this in last checkin.
    git-svn-id: svn+ssh://svn.cm.aol.com/advertising/adlearn/gen1/trunk/lib/cpp/DataProxy@281521 adebfd7e-f233-4696-9a66-a9d0f0efb86a

---
+++ interface/ITransformFunction.hpp
@@ -0,0 +1,27 @@
+//
+// FILE NAME:       $HeadURL: svn+ssh://esaxe@svn.cm.aol.com/advertising/adlearn/gen1/trunk/lib/cpp/DataProxy/StreamTransformer.hpp $
+//
+// REVISION:        $Revision: 281219 $
+//
+// COPYRIGHT:       (c) 2008 Advertising.com All Rights Reserved.
+//
+// LAST UPDATED:    $Date: 2013-06-17 17:17:43 -0400 (Mon, 17 Jun 2013) $
+// UPDATED BY:      $Author: esaxe $
+
+#ifndef _I_TRANFORM_FUNCTION_HPP_
+#define _I_TRANFORM_FUNCTION_HPP_
+
+#include <iostream>
+#include <boost/noncopyable.hpp>
+#include <boost/shared_ptr.hpp>
+
+class ITransformFunction : public boost::noncopyable
+{
+public:
+	ITransformFunction() {};
+	virtual ~ITransformFunction() {};
+
+	virtual boost::shared_ptr<std::istream> TransformInput( boost::shared_ptr< std::istream > i_pInput, const std::map<std::string, std::string>& i_rParameters ) = 0;
+};
+
+#endif // _I_TRANSFORM_FUNCTION
